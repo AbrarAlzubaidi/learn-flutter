664426ea278d749a7668b069c1a3e01b6dcd3b80
AbrarAlzubaidi <abraralzobaide98@gmail.com>
2023-04-02 23:05:32 +0300

    learn how to enable tree of Widgets and how to add buttons

---
+++ notes/lesson_29.md
@@ -0,0 +1,12 @@
+# different type of widgets
+
+1. visible (input and output):
+    - ex: RaisedButton(), Text(), Card(), ...
+    - drawn onto the screen (wha the user sees)
+
+2. invisible (layout and control):
+    - ex: Row(), Column(), ListView(), ...
+    - give your app structure and control how visible widgets are drawn onto the screen
+
+3. container
+    - ex: Container()
--- question_game/lib/main.dart
+++ question_game/lib/main.dart
@@ -4,12 +4,34 @@ void main(List<String> args) {
   runApp(const QuestionApp());
 }
 
+// void main () => runApp(const QuestionApp());
+
 class QuestionApp extends StatelessWidget {
   const QuestionApp({super.key}); // create constructor
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Welcome itnto Question App'),
-    );
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Question App',
+          style: TextStyle(color: Color.fromARGB(255, 10, 10, 10)),
+        ),
+        backgroundColor: const Color.fromARGB(255, 248, 203, 143),
+      ),
+      body: Row(children: const [
+        Text(
+          'the body goes here',
+          style: TextStyle(
+            color: Color(0xFFFFFFFF),
+          ),
+        ),
+        ElevatedButton(
+            onPressed: null,
+            child:
+                Text('click me!', style: TextStyle(color: Color(0xFFFFFFFF)))),
+      ]),
+      backgroundColor: const Color(0xFFFF9000),
+    ));
   }
 }
